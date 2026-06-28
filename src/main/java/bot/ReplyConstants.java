eb307d4ef2ba2be57f6f3ff26e785303de1415c0
tod-eug <toe-eug@yandex.ru>
2026-06-28 10:24:57 +0200

    Russian localisation

--- src/main/java/bot/ReplyConstants.java
+++ src/main/java/bot/ReplyConstants.java
@@ -2,11 +2,19 @@ package bot;
 
 public class ReplyConstants {
 
-    public static String START_REPLY_WELCOME = "Welcome! ";
-    public static String NOT_ALLOWED = "This bot can do nothing for you. Apologies.";
-    public static String GIVE_ME_THE_LINK = "Send me please the video link and will download it. The message should contain only link without any additional text or symbols";
-    public static String TRYING_TO_DOWNLOAD = "Got the link, try to download the video, please wait...";
-    public static String SUCCESSFULLY_DOWNLOADED = "Video is successfully downloaded. Uploading it in this chat. It will appear in couple of moments";
-    public static String ERROR_OCCURRED_WHILE_DOWNLOADING = "Unexpected error occurred.";
-    public static String LINK_IS_INCORRECT = "Given link doesn't look like youtube video link";
+    public static String START_REPLY_WELCOME = "Добро пожаловать! ";
+    public static String NOT_ALLOWED = "К сожалению этот бот ничего не может для вас сделать, извините.";
+    public static String GIVE_ME_THE_LINK = "Пожалуйста отправь мне ссылку на youtube видео одним сообщением. В сообщении должна быть только ссылка на видео, никаких дополнительных символов в сообщении быть не должно, иначе возникнет ошибка.";
+    public static String TRYING_TO_DOWNLOAD = "Ссылка получена, скачиваю видео, пожалуйста подождите...";
+    public static String SUCCESSFULLY_DOWNLOADED = "Видео успешно загружено, загружаю видео в этот чат. Оно появится через несколько мгновений.";
+    public static String ERROR_OCCURRED_WHILE_DOWNLOADING = "Возникла непредвиденная ошибка.";
+    public static String LINK_IS_INCORRECT = "Полученный текст не выглядит как ссылка на youtube видео";
+
+    public static String START_REPLY_WELCOME_EN = "Welcome! ";
+    public static String NOT_ALLOWED_EN = "This bot can do nothing for you. Apologies.";
+    public static String GIVE_ME_THE_LINK_EN = "Send me please the video link and will download it. The message should contain only link without any additional text or symbols";
+    public static String TRYING_TO_DOWNLOAD_EN = "Got the link, try to download the video, please wait...";
+    public static String SUCCESSFULLY_DOWNLOADED_EN = "Video is successfully downloaded. Uploading it in this chat. It will appear in couple of moments";
+    public static String ERROR_OCCURRED_WHILE_DOWNLOADING_EN = "Unexpected error occurred.";
+    public static String LINK_IS_INCORRECT_EN = "Given link doesn't look like youtube video link";
 }
